106aefb4d0d65cd488c93d2a1d8027b3118e0326
renxun <renxun@doupai.cc>
2019-05-06 11:07:34 +0800

    Range

--- awesomeProject/GO_语言范围(Range).go
+++ awesomeProject/GO_语言范围(Range).go
@@ -9,10 +9,21 @@ func main() {
 	// 声明一个切片
 	nums := []int{1,2,3}
 	sum := 0
+	// 如果不需要下标可以用 _ 代替
 	for _,nums := range nums {
 		sum += nums
 	}
 	fmt.Println(sum)
 
+	// range可以应用于map键值对上
+	maps := map[string]string{"a":"apple","b":"banana"}
+	for key,value := range maps {
+		fmt.Println(key,value)
+	}
+
+	// 字符串 i: 索引, c: Unicode的值
+	for i, c := range "go" {
+		fmt.Println(i, c)
+	}
 
 }
